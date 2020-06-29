5bc89bcb77fcb3161e1f8ca7325c3fb36590ea18
Joseph Park <jhspark17@gmail.com>
2020-06-29 16:04:28 -0700

    Add keydown for skipping time

--- script.js
+++ script.js
@@ -13,18 +13,14 @@ function toggleVideoStatus() {
 
 //update play/pause icon
 function updatePlayIcon() {
- if (video.paused) {
-   play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
- } else {
-   play.innerHTML = '<i class="fa fa-pause fa-2x"></i>'
- }
+ if (video.paused) play.innerHTML = '<i class="fa fa-play fa-2x"></i>'
+ else play.innerHTML = '<i class="fa fa-pause fa-2x"></i>'
 }
 
 //update progress & timestamp
 function updateProgress() {
   progress.value = (video.currentTime / video.duration) * 100
-  if (video.currentTime === video.duration) video.currentTime = 0;
- 
+  if (video.currentTime === video.duration) progress.value = 0;
   //Get minutes
   let mins = Math.floor(video.currentTime / 60);
   mins = mins < 10 ? "0" + mins : mins;
@@ -52,16 +48,24 @@ function changeScreen(){
   video.webkitRequestFullscreen()
 }
 
+function skip(e){
+  let time = (video.currentTime / video.duration) 
+  if (e.keyCode === 37) {
+    time = (time * 100) - 2.5 < 0 ? 0 : (time * 100) - 2.5;
+    progress.value = time;
+    setVideoProgress()
+  } else {
+    time = (time * 100) + 2.5 < 0 ? 0 : (time * 100) + 2.5;
+    progress.value = time;
+    setVideoProgress()
+  }
+}
+
 //Event Listeners
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('ended', poster)
 play.addEventListener('click', toggleVideoStatus);
-window.addEventListener('keydown', (e) => {
-  if (e.keyCode === 32) toggleVideoStatus();
-  else if (e.keyCode === 70) changeScreen();
-  else if (e.keyCode === 83) stopVideo();
-  
-})
+
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
 video.addEventListener('timeupdate', updateProgress);
@@ -69,5 +73,11 @@ video.addEventListener('timeupdate', updateProgress);
 stop.addEventListener('click', stopVideo);
 progress.addEventListener('change', setVideoProgress)
 fullScreen.addEventListener('click', changeScreen);
+window.addEventListener('keydown', (e) => {
+  if (e.keyCode === 32) toggleVideoStatus();
+  else if (e.keyCode === 70) changeScreen();
+  else if (e.keyCode === 83) stopVideo();
+  else skip(e);
+})
 
 
